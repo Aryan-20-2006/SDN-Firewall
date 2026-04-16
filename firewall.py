b5b25fc3c25180f8c26256aa43ff8a65280e8d8b
LOSt-GENZ <daringdumplings@gmail.com>
2026-04-17 01:53:38 +0530

    Update firewall.py

--- firewall.py
+++ firewall.py
@@ -6,29 +6,33 @@ log = core.getLogger()
 # Block multiple IPs
 BLOCKED_IPS = ["10.0.0.1", "10.0.0.2"]
 
+
 def _handle_PacketIn(event):
     packet = event.parsed
 
     ip_packet = packet.find('ipv4')
 
-    if ip_packet:
-        src_ip = str(ip_packet.srcip)
+    if not ip_packet:
+        return
 
-        # Log incoming packet
-        log.info("Packet received from %s", src_ip)
+    src_ip = str(ip_packet.srcip)
 
-        # Match condition (source IP)
-        if src_ip in BLOCKED_IPS:
-            # Action: DROP
-            log.info("Action: DROP for %s", src_ip)
-            return
-        else:
-            # Action: FORWARD
-            log.info("Action: FORWARD for %s", src_ip)
+    # Log packet arrival
+    log.info("Packet received from %s", src_ip)
 
-    # Forward packet (flood)
-    msg = of.ofp_packet_out()
-    msg.data = event.ofp
+    # Create flow rule
+    msg = of.ofp_flow_mod()
+    msg.match = of.ofp_match.from_packet(packet)
+
+    # 🚫 BLOCK rule
+    if src_ip in BLOCKED_IPS:
+        log.info("Action: DROP for %s", src_ip)
+        # No action = drop
+        event.connection.send(msg)
+        return
+
+    # ✅ ALLOW rule
+    log.info("Action: FORWARD for %s", src_ip)
     msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
     event.connection.send(msg)
 
@@ -36,6 +40,14 @@ def _handle_PacketIn(event):
 def launch():
     def start_switch(event):
         log.info("Firewall running...")
-        event.connection.addListeners(_handle_PacketIn)
+        connection = event.connection
+
+        # 🔥 IMPORTANT: send packets to controller
+        msg = of.ofp_flow_mod()
+        msg.actions.append(of.ofp_action_output(port=of.OFPP_CONTROLLER))
+        connection.send(msg)
+
+        # Listen for PacketIn events
+        connection.addListeners(_handle_PacketIn)
 
     core.openflow.addListenerByName("ConnectionUp", start_switch)
